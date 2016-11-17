9a1525a4e8a98a0a3a143a3fbbf3afa70e6eab30
procamora <pablojoserocamora@gmail.com>
2016-11-17 01:40:24 +0100

    set_rele ya se puede usar en  2 pasos

--- Domotica-Arduino.ino
+++ Domotica-Arduino.ino
@@ -11,7 +11,7 @@
 #include <WiFiClientSecure.h>
 #include <ESP8266TelegramBOT.h>
 
-#include "credentials.h"  //
+#include "credentials.h"  // todos los valores son String
 
 // Pines de la arduino y temperatura
 #define RELE_UNO D4
@@ -21,21 +21,15 @@
 #define TEMP_FRIO 17.0
 #define LED_FRIO D8
 
+#define TAM_ARRAY 3  // array de string que se le pasa por referencia
+#define CONVERSION_MILLIS 60000  // para conversion minutos a milisegundos
+#define tiempo_espera 2000 //mean time between scan messages
 
-#define TAM_ARRAY 3
-#define CONVERSION_MILLIS 60000
-
-// Initialize Wifi connection to the router
-const char* ssid = SSID_WIFI;
-const char* password = PASS_WIFI;
-
-// Initialize Telegram BOT
-#define BOTtoken API_BOT  //token of TestBOT
-#define BOTname NAME_BOT
-#define BOTusername ALIAS_BOT
+// Inicializamos Telegram BOT
+#define BOTtoken API_BOT  // token bot
+#define BOTname NAME_BOT  // nombre visible del bot
+#define BOTusername ALIAS_BOT  //alias, acaba en ...._bot
 TelegramBOT bot(BOTtoken, BOTname, BOTusername);
-int admin_tg = ID_TELEGRAM;         //PONER EN CREDENTIAL.H
-#define Bot_mtbs 2000 //mean time between scan messages
 
 
 typedef struct {
@@ -59,8 +53,8 @@ void conecta_wifi() {
   Serial.begin(115200);
   Serial.println();
   Serial.print("connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
+  Serial.println(SSID_WIFI);
+  WiFi.begin(SSID_WIFI, PASS_WIFI);  //definidas en credentials.h
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -86,21 +80,7 @@ void setup() {
   global_temporizador.activo = false;
 
   bot.begin();      // launch Bot functionalities
-  bot.sendMessage(String(admin_tg), "Inicio arduino", "");
-}
-
-
-void set_rele(String accion, String user) {
-  String msg = "";
-  if (accion == "on") {
-    digitalWrite(RELE_UNO, HIGH);    // turn the LED off (LOW is the voltage level)
-    msg = "Encendido rele 1";
-  }
-  else if (accion == "off") {
-    digitalWrite(RELE_UNO, LOW);    // turn the LED off (LOW is the voltage level)
-    msg = "Apagado rele 1";
-  }
-  bot.sendMessage(user, msg, "");
+  bot.sendMessage(ID_TELEGRAM, "Inicio arduino", "");
 }
 
 
@@ -113,6 +93,34 @@ void show_start(String user) {
 }
 
 
+void set_rele(String accion, String user) {
+  Serial.println(accion);
+  Serial.println(accion.length());
+  String msg = "parametro de /set_rele incorecto";
+  if (accion == "on") {
+    digitalWrite(RELE_UNO, HIGH);    // turn the LED off (LOW is the voltage level)
+    msg = "Encendido rele 1";
+  }
+  else if (accion == "off") {
+    digitalWrite(RELE_UNO, LOW);    // turn the LED off (LOW is the voltage level)
+    msg = "Apagado rele 1";
+  }
+  //imprimimos mensaje dependiendo de si ha acabado o espera el parametro
+  if (accion.length() == 0) {
+    gobal_tg.set_rele = true;
+    bot.sendMessage(user, "Que acción que deseas hacer con el rele? /on /off", "");
+  }
+  else
+    bot.sendMessage(user, msg, "");
+
+}
+
+
+bool get_rele() {
+  return bool(digitalRead(RELE_UNO));
+}
+
+
 void set_temporizador(String accion, int tiempo, String user) {
   global_temporizador.temporizador = tiempo * CONVERSION_MILLIS;
   global_temporizador.accion_temporizador = accion;
@@ -120,12 +128,10 @@ void set_temporizador(String accion, int tiempo, String user) {
   global_temporizador.activo = true;
   String msg = "Parametro de /set_rele incorrecto";
 
-  if (accion == "on") {
+  if (accion == "on")
     msg = "Programado encendido en " + String(tiempo) + " min";
-  }
-  else if (accion == "off") {
+  else if (accion == "off")
     msg = "Programado apagado en " + String(tiempo) + " min";
-  }
   bot.sendMessage(user, msg, "");
 }
 
@@ -135,6 +141,7 @@ String get_temporizador() {
   return String(tiempo / CONVERSION_MILLIS);
 
 }
+
 
 /**
    @brief funcion para parsear un string y dejar el resultado en un array, parsea el comando y 2 argumentos como maximo
@@ -151,28 +158,46 @@ void parseaComando(String comando, String arrays[TAM_ARRAY]) {
   arrays[0] = comando.substring(0, commaIndex);
   arrays[1] = comando.substring(commaIndex + 1, secondCommaIndex);
   arrays[2] = comando.substring(secondCommaIndex + 1); //+1 para quitar espacio en blanco
+
+  //cuando solo hay 1 parametro el 2 y 3 contienen el primer parametro, asi los dejo vacios
+  if (arrays[1][0] == '/')
+    arrays[1] = "";
+  if (arrays[2][0] == '/')
+    arrays[2] = "";
 }
 
 
 /********************************************
    EchoMessages - function to Echo messages
  ********************************************/
-void Bot_EchoMessages(float temp) {
-  for (int i = 1; i < bot.message[0][0].toInt() + 1; i++)      {
+void botTrataMensajes(float temp) {
+  for (int i = 1; i < bot.message[0][0].toInt() + 1; i++) {
     String comando = bot.message[i][5].substring(1, bot.message[i][5].length()); //elimino el caracter \ del comando
     String parse_comandos[TAM_ARRAY];
     parseaComando(comando, parse_comandos);
 
-    int id_user = (bot.message[i][1]).toInt();
-    //bot.sendMessage(bot.message[i][4], comando, "");
-
     //MODO ADMINISTRADOR
-    if (admin_tg == id_user) {
-      if (parse_comandos[0] == "/get_temp")
+    if (String(ID_TELEGRAM) == String(bot.message[i][1])) {
+      if (gobal_tg.set_rele) {
+        gobal_tg.set_rele = false;
+        //elimino el primer caracter que sera un '/'
+        set_rele(parse_comandos[0].substring(1, parse_comandos[0].length()), bot.message[i][4]);
+      }
+
+      else if (parse_comandos[0] == "/get_temp")
         bot.sendMessage(bot.message[i][4], "La temperatura es: " + String(temp) + "*C", "");
 
       else if (parse_comandos[0] == "/set_rele")
         set_rele(parse_comandos[1], bot.message[i][4]);
+
+      else if (parse_comandos[0] == "/get_rele") {
+        String msg;
+        if (get_rele())
+          msg = "Encendido";
+        else
+          msg = "Apagado";
+        bot.sendMessage(bot.message[i][4], "Estado del rele: " + msg, "");
+      }
 
       else if (parse_comandos[0] == "/set_temporizador")
         set_temporizador(parse_comandos[1], parse_comandos[2].toInt(), bot.message[i][4]);
@@ -190,7 +215,7 @@ void Bot_EchoMessages(float temp) {
       }
 
       else
-        bot.sendMessage(bot.message[i][4], "Comando desconocido", "");
+        bot.sendMessage(bot.message[i][4], "Comando desconocido :(", "");
     }
 
     //MODO INVITADO
@@ -205,7 +230,7 @@ void Bot_EchoMessages(float temp) {
 }
 
 
-void AjustaLedTemperatura(float temperatura) {
+void ajustaLedTemperatura(float temperatura) {
   if (temperatura > TEMP_CALOR) {
     digitalWrite(LED_FRIO, LOW);
     digitalWrite(LED_CALOR, HIGH);
@@ -231,13 +256,13 @@ void loop() {
       global_temporizador.activo = false;
     }
 
-  if (millis() > global_temporizador.Bot_lasttime + Bot_mtbs)  {
+  if (millis() > global_temporizador.Bot_lasttime + tiempo_espera)  {
     global_temporizador.Bot_lasttime = millis();
 
     float t = 0;
 
     bot.getUpdates(bot.message[0][1]);   // launch API GetUpdates up to xxx message
-    Bot_EchoMessages(t);   // reply to message with Echo
+    botTrataMensajes(t);   // reply to message with Echo
   }
 }
 
